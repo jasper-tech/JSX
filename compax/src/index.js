74ce2cc4329dad1386cccc69f3985f790c5a7f64
jasper tech <curdyj78@gmail.com>
2023-06-08 11:32:49 -1200

    index update

--- compax/src/index.js
+++ compax/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from './App'; 
+
+
+const root = ReactDOM.createRoot( document.getElementById('root'));
+function ref(){
+    root.render(<App/>)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));      
