71d492ce8a6a6d8d91078ba56518364607d8aef9
jasper tech <curdyj78@gmail.com>
2023-06-09 00:34:45 -1200

    try

--- compax/src/index.js
+++ compax/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from './App'; 
 
 
-const root = ReactDOM.createRoot( document.getElementById('root'));
+ const root = ReactDOM.createRoot( document.getElementById('root'));
 function ref(){
     root.render(<App/>)
 }
